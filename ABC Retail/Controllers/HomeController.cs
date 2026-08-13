29c40df855aec5aed278d82c5a8c9f6cb4fb7b62
Koketso Masasanya <ST10225793@rcconnect.edu.za>
2026-08-13 10:07:27 +0200

    Change the dashboard layout

--- ABC Retail/Controllers/HomeController.cs
+++ ABC Retail/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using ABC_Retail.Models;
+using ABC_Retail.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -6,35 +7,47 @@ namespace ABC_Retail.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly TableService _tableService;
+        private readonly BlobService _blobService;
+        private readonly QueueService _queueService;
+        private readonly FileService _fileService;
+
+        public HomeController(
+            TableService tableService,
+            BlobService blobService,
+            QueueService queueService,
+            FileService fileService)
         {
-            return View();
+            _tableService = tableService;
+            _blobService = blobService;
+            _queueService = queueService;
+            _fileService = fileService;
         }
 
-        public IActionResult Profiles()
+        public async Task<IActionResult> Index()
         {
-            return RedirectToAction("Index", "Profiles");
+            var customers = await _tableService.GetAllCustomersAsync();
+            var products = await _blobService.GetAllProductsAsync();
+            var queueMessages = await _queueService.PeekMessagesAsync();
+            var logFiles = await _fileService.ListFilesAsync();
+
+            var viewModel = new DashboardViewModel
+            {
+                TotalCustomers = customers.Count,
+                TotalProducts = products.Count,
+                TotalQueueMessages = queueMessages.Count,
+                TotalLogFiles = logFiles.Count,
+                RecentQueueEvents = queueMessages.Take(5).ToList()
+            };
+
+            return View(viewModel);
         }
 
-        public IActionResult Products()
-        {
-            return RedirectToAction("Index", "Products");
-        }
-
-        public IActionResult Inventory()
-        {
-            return RedirectToAction("Index", "Orders");
-        }
-
-        public IActionResult Logs()
-        {
-            return RedirectToAction("Index", "Logs");
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+        public IActionResult Profiles() => RedirectToAction("Index", "Profiles");
+        public IActionResult Products() => RedirectToAction("Index", "Products");
+        public IActionResult Inventory() => RedirectToAction("Index", "Orders");
+        public IActionResult Logs() => RedirectToAction("Index", "Logs");
+        public IActionResult Privacy() => View();
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ ABC Retail/Models/DashboardViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ABC_Retail.Models
+{
+    public class DashboardViewModel
+    {
+        public int TotalCustomers { get; set; }
+        public int TotalProducts { get; set; }
+        public int TotalQueueMessages { get; set; }
+        public int TotalLogFiles { get; set; }
+        public List<QueueMessageModel> RecentQueueEvents { get; set; } = new List<QueueMessageModel>();
+    }
+}
